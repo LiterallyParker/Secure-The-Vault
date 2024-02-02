c51761b07efe36818859130bb189335702e6d4cc
LiterallyParker <parkernash2001@gmail.com>
2024-02-01 19:05:25 -0600

    Final Product

--- secureTheVault.js
+++ secureTheVault.js
@@ -1,9 +1,16 @@
+// create first code value
 let num1 = 5+5
 
+// create second code value
 let num2 = num1*(12-8)
 
+// create third code value
 let num3 = num2 - 1
 
+// create code message
 let code = `${num1} - ${num2} - ${num3}`
 
-window.alert(`The code is ${code}`)
+// popup code
+window.alert(`You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: ${code}`)
+
+// sorry i submitted this so fast without checking the rubric! got excited about javascript lol
